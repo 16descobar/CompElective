fd76cb0b8280db5123634ea7acbf9ec47e563d80
16descobar <16descobar@iaa.edu.ec>
2015-05-08 08:20:05 -0500

    Create Variables.py

---
+++ Variables.py
@@ -0,0 +1,11 @@
+name = 'Daniel Escobar'
+age = 16
+gpa = 4.0
+
+print name
+print age
+print gpa
+
+print 'My name is',name,', I am ',age,' old. My GPA is:', gpa,'.'
+print 'In ten years, I will be', age +10
+print 'Age in dog years is', age*7
