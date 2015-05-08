02b3da22d2e59a09d06124aab67219934dce6f00
16descobar <16descobar@iaa.edu.ec>
2015-05-08 08:21:01 -0500

    Create Practice1.py

---
+++ Practice1.py
@@ -0,0 +1,14 @@
+Name = "Daniel Escobar"
+Age = 16
+PhoneNumber = 55555555
+Address = "1st Street, 2nd avenue"
+Email = "email@email.com"
+
+print Name
+print Age
+print PhoneNumber
+print Address
+print Email
+
+print "My name is ", Name, ". I am",age ,"years old. My Number is" PhoneNumber ". I live in "Address". My email is " email "."
+
