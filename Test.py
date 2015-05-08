e282269cf1a68dd1f664593ea10c2eeee222ce7c
16descobar <16descobar@iaa.edu.ec>
2015-05-08 08:20:41 -0500

    Create Test.py

---
+++ Test.py
@@ -0,0 +1,15 @@
+operand1 = 95
+operand2 = 65.65
+
+print "95 + 64.5 = ", operand1+operand2 
+print "95 - 64.5 = ", operand1-operand2 
+
+print "95 x 64.5 = ", operand1*operand2 
+
+print "95 / 64.5 = ", operand1+operand2 
+
+print "95 % 64.5 = ", operand1%operand2
+
+
+#Variables created annd assigned Variables
+
