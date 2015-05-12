bed12137047a6b85ec6c31316ed82c0043f75f59
16descobar <16descobar@iaa.edu.ec>
2015-05-12 09:01:09 -0500

    Create Practice2.py

---
+++ Practice2.py
@@ -0,0 +1,8 @@
+import sys
+
+print "My name is ", sys.arv [1]
+print "My Lastname is " sys.arv [2]
+print "My birthday is " sys.arv [3]
+print "My favorite band is " sys.arv [4]
+print "My favorite movie is " sys.arv [5]
+
